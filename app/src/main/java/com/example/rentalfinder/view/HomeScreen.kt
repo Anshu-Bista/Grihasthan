62a8b7164feb709a05a692af529b90d7540bd36d
Dawn <bistanshu3@gmail.com>
2025-12-26 13:12:47 +0545

    added screens in dashboard

---
+++ app/src/main/java/com/example/rentalfinder/view/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.rentalfinder.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+    Column { Text("Hello") }
+}
---
+++ app/src/main/java/com/example/rentalfinder/view/SearchScreen.kt
@@ -0,0 +1,11 @@
+package com.example.rentalfinder.view
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen(){
+    Column { Text("Hello") }
+}

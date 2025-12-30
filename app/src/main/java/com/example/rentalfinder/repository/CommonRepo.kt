bf5d1e21270e5f77352ea353383135efafe08f0c
Dawn <bistanshu3@gmail.com>
2025-12-30 16:47:21 +0545

    added common repo and implemented it

---
+++ app/src/main/java/com/example/rentalfinder/repository/CommonRepo.kt
@@ -0,0 +1,17 @@
+package com.example.rentalfinder.repository
+
+import android.content.Context
+import android.net.Uri
+
+interface CommonRepo {
+    fun uploadImage(
+        context: Context,
+        imageUri: Uri,
+        callback:(String?)-> Unit
+    )
+
+    fun getFileNameFromUri(
+        context: Context,
+        imageUri: Uri
+    ): String?
+}
---
+++ app/src/main/java/com/example/rentalfinder/repository/CommonRepoImpl.kt
@@ -0,0 +1,77 @@
+package com.example.rentalfinder.repository
+
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.OpenableColumns
+import com.cloudinary.Cloudinary
+import com.cloudinary.utils.ObjectUtils
+import java.io.InputStream
+import java.util.concurrent.Executors
+
+class CommonRepoImpl: CommonRepo {
+    private val cloudinary = Cloudinary(
+        mapOf(
+            "cloud_name" to "dcfojlbp9",
+            "api_key" to "466777364614965",
+            "api_secret" to "NBS8YsxpfEraxVufHF59VXguezU"
+        )
+    )
+
+    override fun uploadImage(
+        context: Context, imageUri: Uri,
+        callback: (String?) -> Unit
+    ) {
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            try {
+                val inputStream: InputStream? = context.contentResolver.openInputStream(imageUri)
+                var fileName = getFileNameFromUri(context, imageUri)
+
+                fileName = fileName?.substringBeforeLast(".") ?: "uploaded_image"
+
+                val response = cloudinary.uploader().upload(
+                    inputStream, ObjectUtils.asMap(
+                        "public_id", fileName,
+                        "resource_type", "image"
+                    )
+                )
+
+                var imageUrl = response["url"] as String?
+
+                imageUrl = imageUrl?.replace("http://", "https://")
+
+                Handler(Looper.getMainLooper()).post {
+                    callback(imageUrl)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Handler(Looper.getMainLooper()).post {
+                    callback(null)
+                }
+            }
+        }
+    }
+
+
+    override fun getFileNameFromUri(
+        context: Context, uri: Uri
+    ): String? {
+        var fileName: String? = null
+        val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (nameIndex != -1) {
+                    fileName = it.getString(nameIndex)
+                }
+            }
+        }
+        return fileName
+    }
+
+
+}

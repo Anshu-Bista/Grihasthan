e8f870d115130dc9f44b2eed67e03621548e934d
Dawn <bistanshu3@gmail.com>
2026-01-08 09:20:22 +0545

    property model

---
+++ app/src/main/java/com/example/rentalfinder/model/PropertyModel.kt
@@ -0,0 +1,77 @@
+package com.example.rentalfinder.model
+
+class PropertyModel(
+    // Basic info
+    val id: String = "",
+    val title: String = "",
+    val price: Double = 0.0,
+    val totalArea: Double = 0.0,
+    val description: String = "",
+
+    // Category
+    val categoryId: String = "",
+
+    // Location
+    val city: String = "",
+    val location: String = "",
+    val streetAddress: String = "",
+    val zipCode: Int = 0,
+
+    // Amenities
+    val amenities: List<String> = emptyList(),
+
+    // Property details
+    val leaseType: String = "",
+    val furnitureType: String = "",
+    val tenantType: String = "",
+    val yearBuilt: Int = 0,
+    val levels: Int = 0,
+    val bedrooms: Int = 0,
+    val bathrooms: Int = 0,
+    val kitchens: Int = 0,
+
+    // Images
+    var imageUrl :String = "",
+    ) {
+
+
+    fun PropertyModel.toMap(): Map<String, Any?> {
+        return mapOf(
+            // Basic info
+            "id" to id,
+            "title" to title,
+            "price" to price,
+            "totalArea" to totalArea,
+            "description" to description,
+
+            // Category
+            "categoryId" to categoryId,
+
+            // Location (nested map is BEST practice)
+            "location" to mapOf(
+                "city" to city,
+                "area" to location,
+                "streetAddress" to streetAddress,
+                "zipCode" to zipCode
+            ),
+
+            // Amenities
+            "amenities" to amenities,
+
+            // Property details
+            "leaseType" to leaseType,
+            "furnitureType" to furnitureType,
+            "tenantType" to tenantType,
+            "yearBuilt" to yearBuilt,
+            "levels" to levels,
+            "bedrooms" to bedrooms,
+            "bathrooms" to bathrooms,
+            "kitchens" to kitchens,
+
+            // Images
+            "imageUrl" to imageUrl
+        )
+    }
+
+
+}
